fcb59ed7c597e18c5649a3ee1545f785b3d8f80b
djmcgre <davisjm2@illinois.edu>
2019-10-17 12:51:24 -0500

    comments added

--- tri_challenge_rank.py
+++ tri_challenge_rank.py
@@ -13,13 +13,14 @@ SWIM_TIME = 1.5
 BIKE_TIME = 6.5
 RUN_TIME = 3.5
 
-file = 'Illini Triathlon Challenge.xlsx'
-wb = xlrd.open_workbook(file)
+wb_name = 'Illini Triathlon Challenge.xlsx'
+wb = xlrd.open_workbook(wb_name)
 df = pd.DataFrame(columns=['athlete', 'swim_mi', 'bike_mi', 'run_mi', 'time_left_hrs'])
 
 for index in np.arange(wb.nsheets):
     sheet = wb.sheet_by_index(index)
     athlete = sheet.name
+    #(row,col) positions where totals are stored
     swim = sheet.cell_value(27,16)/4200*2.4 #convert to a fraction of 2.4 mi (actual conversion is 1760 yd/mi)
     bike = sheet.cell_value(28,16)
     run = sheet.cell_value(29,16)
